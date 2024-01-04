06f000b292c70de3e5a339ac2a823130f5524ae7
Grégory PAUL <gregory.paul@mappy.com>
2024-01-04 17:01:24 +0100

    chore: add deprecation message

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-const { ConsentString } = require('consent-string')
-const input = document.querySelector('input[type=text]')
-const output = document.querySelector('.output')
+const { ConsentString } = require("consent-string");
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
 
-const decode =  (e) => {
-
-    try {
-        output.classList.remove('error')
-        if (!input.value) {
-            output.textContent = 'Nothing to decode'
-        } else {
-            const consent = new ConsentString(input.value.replace(/"/g, ''))
-            output.textContent = JSON.stringify(consent, null, 2)
-        }
-    } catch (e) {
-        output.textContent = e
-        output.classList.add('error')
+const decode = (e) => {
+  try {
+    output.classList.remove("error");
+    if (!input.value) {
+      output.textContent = "Nothing to decode";
+    } else {
+      const consent = new ConsentString(input.value.replace(/"/g, ""));
+      output.textContent = JSON.stringify(consent, null, 2);
     }
-}
+  } catch (e) {
+    output.textContent = e;
+    output.classList.add("error");
+  }
+};
 
-input.addEventListener('keyup', decode)
-decode()
+input.addEventListener("keyup", decode, false);
+decode();
